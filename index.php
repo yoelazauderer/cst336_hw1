4d30ed01eeb16d06e443b10894f983a9800bade5
EC2 Default User <ec2-user@ip-172-31-8-36.us-west-1.compute.internal>
2020-11-02 21:39:06 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    
+    <!-- Color Scheme -->
+        <!--Dark gray: #737373-->
+        <!--Light gray: #d9d9d9-->
+        <!--Blue: #38b6ff-->
+        <!--Green: 7ed957-->
+    
+    <head>
+        <meta charset="utf-8"/>
+        <title> Computer Science Institute </title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>
+        
+        <div id="headersection">
+        <div id="logo-image">
+        <a href="index.html">
+            <img src="img/csi-logo-transparent.png" alt="CSI Logo" />
+        </a>
+        </div>
+       
+        <nav>
+             
+            <nav>
+            <a href="java.html"> JAVA </a>
+            <a href="c++.html"> C++ </a>
+            <a href="python.html"> PYTHON </a>
+            
+        </nav>
+            
+        </nav>
+       </div>
+       <br/> <br/>
+       
+       <div id="content">
+            <h2>Click on a language below to learn more!</h2>
+            </br></br>
+            <div class="logos">
+                <a class="logos-item" href="java.html">
+                    <img src="img/java-logo-1.png" alt="Java Logo" /></a>
+                <a class="logos-item" href="python.html">
+                    <img src="img/c++-logo.png" alt="C++ Logo" /></a>
+                <a class="logos-item" href="c++.html">
+                    <img src="img/Python-Logo-1.png" alt="Python Logo" /></a>
+               </div>
+               </br></br></br></br>
+       </div>
+       
+       
+        <footer>
+            
+            <div id="footersection">
+            
+            <figure>
+                <img src="img/csi-logo-transparent.png" alt="CSI Logo" />
+            </figure>
+            
+            <div>
+            CST336 Internet Programming. 2020&copy; Zauderer
+            <br/>
+            <strong>Disclaimer:</strong> The information in this webpage is fictitious.
+            <br/>
+            It is used for academic purposes only.
+            </div>
+            
+            </div>
+        </footer>
+        
+
+    </body>
+</html>
